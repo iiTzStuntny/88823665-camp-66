f96ea79e623d355309e659cbecac3599c85a799c
Narich Pradit <Narich6866@gmail.com>
2025-02-11 19:17:59 +0700

    Update login.blade.php

--- mylaravel/resources/views/login.blade.php
+++ mylaravel/resources/views/login.blade.php
@@ -4,7 +4,7 @@
 <div class="login-page">
     <div class="login-box">
         <div class="login-logo">
-            <a href="../index2.html"><b>Admin</b>LTE</a>
+            <a href="../index2.html"><b>CAMP</b>66</a>
         </div>
         <!-- /.login-logo -->
         <div class="card">
