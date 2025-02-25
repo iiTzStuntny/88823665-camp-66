9df2473ef8d9d928d2847873e57d6f412bfd6289
Narich Pradit <Narich6866@gmail.com>
2025-02-26 00:08:42 +0700

    เสร็จ

--- mylaravel/app/Http/Controllers/LoginController.php
+++ mylaravel/app/Http/Controllers/LoginController.php
@@ -2,13 +2,43 @@
 
 namespace App\Http\Controllers;
 
+use App;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\Auth;
+
 
 class LoginController extends Controller
 {
+
     //
     function index()
     {
         return view('login');
+    }
+
+    public function login(Request $request)
+    {
+        $user = User::where('email', $request->email)->first();
+        if($user != null && Hash::check($request->password, $user->password)){
+            session(['user' => $user]);
+            return redirect('/home');
+        }else{
+            return redirect('/login');
+        }
+    }
+
+    public function logout()
+    {
+        
+        session()->forget('user');
+        session()->flush();
+        return redirect('/login'); // กลับไปหน้า Login หลังจาก Logout
+    }
+    protected function redirectTo()
+    {
+        return '/home';
     }
 }
---
+++ mylaravel/resources/views/product.blade.php
@@ -0,0 +1,83 @@
+@extends('layouts.default')
+
+@section('content')
+<div class="app-wrapper">
+    @include('components.header')
+    @include('components.menu')
+    <div class = "p-3">
+    <form action="{{ url('/product') }}" method="post">
+        @csrf
+        <div class="row">
+            <div class="col-6">
+                <div class="mb-3">
+                    <label for="category" class="form-label">Category Name</label>
+                    <input type="text" name="category" class="form-control" id="category">
+                </div>
+            </div>
+        </div>
+        <button type="button" id="btn-add-product" class="btn btn-primary">
+            + เพิ่ม product
+        </button>
+        <div class="row" id='add-product'>
+
+        </div>
+        <div class="d-flex justify-content-end mt-3 row ">
+            <button class="btn btn-success col-1 " type="submit">บันทึก</button>
+        </div>
+    </form>
+    <table class="mt-3 table">
+        <thead>
+            <tr>
+                <td>#</td>
+                <td>Category Name</td>
+                <td>Product Name</td>
+                <td>User Name</td>
+            </tr>
+        </thead>
+        <tbody>
+
+            <?php foreach ($categorys as $index => $category) {?>
+            <tr>
+                <td>{{$index + 1}}</td>
+                <td>{{$category->name}}</td>
+                <td>
+                <?php
+                $name= '';
+                foreach ($products->where('category_id', $category->id) as $product) {
+                    $name  = $user->where('id',$product->user_id)->first()->name;
+                    ?>
+                    <ul>
+                        <li>{{$product->name}}</li>
+                    </ul>
+                    <?php } ?>
+                </td>
+                <td>{{$name}}</td>
+            </tr>
+            <?php }?>
+        </tbody>
+    </table>
+</div>
+</div>
+@endsection
+
+@section('scripts')
+<script>
+    $(document).ready(function(){
+        var count =1;
+        $('#btn-add-product').on('click', function(){
+            $("#add-product").append(`
+            <div class="mt-3 col-6">
+                <label class="form-label product-label">${count++}. Product Name
+                    <button type="button" class="btn btn-danger btn-delete-product">ลบ</button>
+                </label>
+                    <input type="text" name="product_name[]" class="form-control">
+            </div>
+            `)
+        })
+
+        $(document).on('click','.btn-delete-product', function(){
+            $(this).parent().parent().remove();
+        })
+    });
+</script>
+@endsection
